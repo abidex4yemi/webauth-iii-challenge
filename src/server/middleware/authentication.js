c26100f077383b9ca43e0ecbaebb70e210c5ab5a
abidex4yemi <abidex4yemi@gmail.com>
2019-07-24 17:55:10 +0100

    Implement json webtoken

---
+++ src/server/middleware/authentication.js
@@ -0,0 +1,73 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../model');
+const { createError, UNAUTHORIZED, GENERIC_ERROR } = require('../util/error.js');
+
+/**
+   * Generate token based on payload.
+   *
+   * @param {*} id
+   * @param {*} isAdmin
+   */
+const generateToken = ({ payload = {}, options = {} }) => {
+  const secretKey = process.env.SECRET_KEY;
+
+  const token = jwt.sign(payload, secretKey, options);
+
+  return token;
+};
+
+/**
+   * Verifies user provided token
+   *
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+const verifyToken = async (req, res, next) => {
+  const token = req.headers.Authorization;
+  const secretKey = process.env.SECRET_KEY;
+
+  // check if token is provided
+  if (!token) {
+    return next(
+      createError({
+        status: UNAUTHORIZED,
+        message: 'Unauthorized!, you have to login',
+      }),
+    );
+  }
+
+  try {
+    // verify user provided token against existing token
+    const decoded = jwt.verify(token, secretKey);
+
+    const user = await User.getById(decoded.id);
+
+    // check for valid app users
+    if (!user) {
+      return next(
+        createError({
+          status: 401,
+          error: 'The token you provided is invalid',
+        }),
+      );
+    }
+
+    // makes user object available through request object
+    req.user = decoded;
+
+    return next();
+  } catch (errors) {
+    return next(
+      createError({
+        message: 'Could not verify token',
+        status: GENERIC_ERROR,
+      }),
+    );
+  }
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
